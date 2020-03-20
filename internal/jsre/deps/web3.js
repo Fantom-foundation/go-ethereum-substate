9c8bc9bca90016af4fd5958578dfc41f0c89a36a
devintegral3 <devintegral3@sfxdx.ru>
2020-03-20 20:37:51 +0300

    Fix error in output formatter

--- internal/jsre/deps/web3.js
+++ internal/jsre/deps/web3.js
@@ -3937,7 +3937,7 @@ var outputStakerFormatter = function(data) {
 var outputStakersFormatter = function(data) {
     if (utils.isArray(data)) {
         data.forEach(function(item) {
-          if (!isString(item)) {
+          if (typeof(item) != 'string') {
             item = outputStakerFormatter(item)
           }
           return item
