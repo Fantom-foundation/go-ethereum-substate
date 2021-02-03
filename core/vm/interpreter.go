2a6ce5ce69cace4123853b93d78d4ff77a8641b3
Egor Lysenko <uprendis@gmail.com>
2021-02-03 18:20:15 +0700

    evm: add precompiled state contracts

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -33,6 +33,8 @@ type Config struct {
 	NoBaseFee               bool  // Forces the EIP-1559 baseFee to 0 (needed for 0 price calls)
 	EnablePreimageRecording bool  // Enables recording of SHA3/keccak preimages
 	ExtraEips               []int // Additional EIPS that are to be enabled
+
+	StatePrecompiles map[common.Address]PrecompiledStateContract // Added by Fantom for custom precompiled contract
 }
 
 // ScopeContext contains the things that are per-call, such as stack and memory,
