fa20486660d0bb198474c41e4b2deba84a553236
HonzaDajc <jan.dajc@gmail.com>
2022-11-04 14:07:23 +0100

    Remove unused comments and reformat

--- core/vm/lfvm/gas.go
+++ core/vm/lfvm/gas.go
@@ -27,7 +27,7 @@ func init() {
 }
 
 func initBerlinGasPrice() {
-	// Changed static gas prices
+	// Changed static gas prices with EIP2929
 	static_gas_prices_berlin[SLOAD] = 0
 	static_gas_prices_berlin[EXTCODECOPY] = 100
 	static_gas_prices_berlin[EXTCODESIZE] = 100
@@ -38,24 +38,6 @@ func initBerlinGasPrice() {
 	static_gas_prices_berlin[STATICCALL] = 100
 	static_gas_prices_berlin[DELEGATECALL] = 100
 	static_gas_prices_berlin[SELFDESTRUCT] = 5000
-
-	// Changed dynamic gas prices for these opcodes
-	// implemented directly in instructions
-	//
-	// jt[SSTORE].dynamicGas = gasSStoreEIP2929
-	// jt[SLOAD].dynamicGas = gasSLoadEIP2929
-	// jt[EXTCODECOPY].dynamicGas = gasExtCodeCopyEIP2929
-	// jt[EXTCODESIZE].dynamicGas = gasEip2929AccountCheck
-	// jt[EXTCODEHASH].dynamicGas = gasEip2929AccountCheck
-	// jt[BALANCE].dynamicGas = gasEip2929AccountCheck
-	// jt[CALL].dynamicGas = gasCallEIP2929
-	// jt[STATICCALL].dynamicGas = gasStaticCallEIP2929
-	// jt[DELEGATECALL].dynamicGas = gasDelegateCallEIP2929
-	// jt[SELFDESTRUCT].dynamicGas = gasSelfdestructEIP2929
-
-	// this instruction is not used sofar for LFVM
-	// jt[CALLCODE].constantGas = params.WarmStorageReadCostEIP2929
-	// jt[CALLCODE].dynamicGas = gasCallCodeEIP2929
 }
 
 func getStaticGasPrice(op OpCode, isBerlin bool) uint64 {
@@ -312,19 +294,19 @@ func gasSStore(c *context) (uint64, error) {
 	return gasSStoreEIP2200(c)
 }
 
-// 0. If *gasleft* is less than or equal to 2300, fail the current call.
-// 1. If current value equals new value (this is a no-op), SLOAD_GAS is deducted.
-// 2. If current value does not equal new value:
-//   2.1. If original value equals current value (this storage slot has not been changed by the current execution context):
+//  0. If *gasleft* is less than or equal to 2300, fail the current call.
+//  1. If current value equals new value (this is a no-op), SLOAD_GAS is deducted.
+//  2. If current value does not equal new value:
+//     2.1. If original value equals current value (this storage slot has not been changed by the current execution context):
 //     2.1.1. If original value is 0, SSTORE_SET_GAS (20K) gas is deducted.
 //     2.1.2. Otherwise, SSTORE_RESET_GAS gas is deducted. If new value is 0, add SSTORE_CLEARS_SCHEDULE to refund counter.
-//   2.2. If original value does not equal current value (this storage slot is dirty), SLOAD_GAS gas is deducted. Apply both of the following clauses:
+//     2.2. If original value does not equal current value (this storage slot is dirty), SLOAD_GAS gas is deducted. Apply both of the following clauses:
 //     2.2.1. If original value is not 0:
-//       2.2.1.1. If current value is 0 (also means that new value is not 0), subtract SSTORE_CLEARS_SCHEDULE gas from refund counter.
-//       2.2.1.2. If new value is 0 (also means that current value is not 0), add SSTORE_CLEARS_SCHEDULE gas to refund counter.
+//     2.2.1.1. If current value is 0 (also means that new value is not 0), subtract SSTORE_CLEARS_SCHEDULE gas from refund counter.
+//     2.2.1.2. If new value is 0 (also means that current value is not 0), add SSTORE_CLEARS_SCHEDULE gas to refund counter.
 //     2.2.2. If original value equals new value (this storage slot is reset):
-//       2.2.2.1. If original value is 0, add SSTORE_SET_GAS - SLOAD_GAS to refund counter.
-//       2.2.2.2. Otherwise, add SSTORE_RESET_GAS - SLOAD_GAS gas to refund counter.
+//     2.2.2.1. If original value is 0, add SSTORE_SET_GAS - SLOAD_GAS to refund counter.
+//     2.2.2.2. Otherwise, add SSTORE_RESET_GAS - SLOAD_GAS gas to refund counter.
 func gasSStoreEIP2200(c *context) (uint64, error) {
 	//fmt.Printf("LFVM: Computing SSTORE costs based on EIP2200 rules ..\n")
 	// If we fail the minimum gas availability invariant, fail (0)
