b6a77a87961166396fac1507f3062cbd1d76bb21
HonzaDajc <jan.dajc@gmail.com>
2023-02-06 13:56:27 +0100

    Add mockStateDB and its call to opcode tests

--- core/vm/lfvm/interpreter_test.go
+++ core/vm/lfvm/interpreter_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/golang/mock/gomock"
 	"github.com/holiman/uint256"
 )
 
@@ -19,20 +20,22 @@ type example struct {
 	function uint32 // The identifier of the function in the contract to be called
 }
 
-func getContext() context {
+const MAX_STACK_SIZE int = 1024
+
+func getEmptyContext() context {
+	code := make([]Instruction, 0)
+	data := make([]byte, 0)
+	return getContext(code, data, 0, nil, false, false)
+}
+
+func getContext(code Code, data []byte, stackPtr int, stateDB vm.StateDB, isBerlin bool, isLondon bool) context {
 
 	// Create a dummy contract
 	addr := vm.AccountRef{}
 	contract := vm.NewContract(addr, addr, big.NewInt(0), 1<<63)
 
-	var exampleCode = []byte{
-		byte(vm.PUSH4), 4}
-	clearConversionCache()
-	code, _ := Convert(common.Address{}, exampleCode, false, 0, false)
-	data := make([]byte, 4+32)
-
 	// Create execution context.
-	return context{
+	ctxt := context{
 		code:     code,
 		data:     data,
 		callsize: *uint256.NewInt(uint64(len(data))),
@@ -40,12 +43,23 @@ func getContext() context {
 		memory:   NewMemory(),
 		readOnly: true,
 		contract: contract,
+		stateDB:  stateDB,
+		isBerlin: isBerlin,
+		isLondon: isLondon,
+		evm:      &vm.EVM{StateDB: stateDB},
 	}
+
+	// Set start conditions
+	ctxt.pc = 0
+	ctxt.status = RUNNING
+	ctxt.stack.stack_ptr = stackPtr
+
+	return ctxt
 }
 
 // Test UseGas function and correct status after running out of gas
 func TestGasFunc(t *testing.T) {
-	ctx := getContext()
+	ctx := getEmptyContext()
 	ctx.contract.Gas = 100
 	ok := ctx.UseGas(10)
 	if !ok {
@@ -67,13 +81,14 @@ func TestGasFunc(t *testing.T) {
 }
 
 type OpcodeTest struct {
-	name      string
-	code      []Instruction
-	fullStack bool
-	argData   uint16
-	endStatus Status
-	isBerlin  bool
-	isLondon  bool
+	name        string
+	code        []Instruction
+	stackPtrPos int
+	argData     uint16
+	endStatus   Status
+	isBerlin    bool
+	isLondon    bool
+	mockCalls   func(mockStateDB *vm.MockStateDB)
 }
 
 func getInstructions(start OpCode, end OpCode) (opCodes []OpCode) {
@@ -115,7 +130,7 @@ func addFullStackFailOpCodes(tests []OpcodeTest) []OpcodeTest {
 	opCodes = append(opCodes, getInstructions(PUSH1, PUSH32)...)
 	opCodes = append(opCodes, getInstructions(DUP1, DUP16)...)
 	for _, opCode := range opCodes {
-		addedTests = append(addedTests, OpcodeTest{opCode.String() + " Stack overflow", []Instruction{{opCode, 1}}, true, 0, ERROR, false, false})
+		addedTests = append(addedTests, OpcodeTest{opCode.String() + " Stack overflow", []Instruction{{opCode, 1}}, MAX_STACK_SIZE, 0, ERROR, false, false, nil})
 	}
 	return addedTests
 }
@@ -129,16 +144,12 @@ func addEmptyStackFailOpCodes(tests []OpcodeTest) []OpcodeTest {
 	opCodes = append(opCodes, getInstructions(SWAP1, SWAP16)...)
 	opCodes = append(opCodes, getInstructions(LOG0, LOG4)...)
 	for _, opCode := range opCodes {
-		addedTests = append(addedTests, OpcodeTest{opCode.String() + " Stack underflow", []Instruction{{opCode, 1}}, false, 0, ERROR, false, false})
+		addedTests = append(addedTests, OpcodeTest{opCode.String() + " Stack underflow", []Instruction{{opCode, 1}}, 0, 0, ERROR, false, false, nil})
 	}
 	return addedTests
 }
 
 func TestStackBoundry(t *testing.T) {
-
-	// Create a dummy contract
-	addr := vm.AccountRef{}
-	contract := vm.NewContract(addr, addr, big.NewInt(0), 1<<63)
 
 	// Add tests for execution
 	tests := addFullStackFailOpCodes([]OpcodeTest{})
@@ -147,25 +158,9 @@ func TestStackBoundry(t *testing.T) {
 	for _, test := range tests {
 
 		// Create execution context.
-		ctxt := context{
-			code:     test.code,
-			data:     nil,
-			callsize: *uint256.NewInt(uint64(0)),
-			stack:    NewStack(),
-			memory:   NewMemory(),
-			readOnly: true,
-			contract: contract,
-		}
-
-		// Reset the context.
-		ctxt.pc = 0
-		ctxt.status = RUNNING
-		ctxt.contract.Gas = 1 << 31
-		if test.fullStack {
-			ctxt.stack.stack_ptr = len(ctxt.stack.data)
-		} else {
-			ctxt.stack.stack_ptr = 0
-		}
+		ctxt := getEmptyContext()
+		ctxt.code = test.code
+		ctxt.stack.stack_ptr = test.stackPtrPos
 
 		// Run testing code
 		run(&ctxt)
@@ -181,7 +176,29 @@ func TestStackBoundry(t *testing.T) {
 }
 
 var opcodeTests = []OpcodeTest{
-	{"POP OK", []Instruction{{PUSH1, 1}, {POP, 0}}, false, 0, STOPPED, false, false},
+	{"POP OK", []Instruction{{PUSH1, 1 << 8}, {POP, 0}}, 0, 0, STOPPED, false, false, nil},
+	{"JUMP OK", []Instruction{{PUSH1, 2 << 8}, {JUMP, 0}, {JUMPDEST, 0}}, 0, 0, STOPPED, false, false, nil},
+	{"JUMPI OK", []Instruction{{PUSH1, 1 << 8}, {PUSH1, 3 << 8}, {JUMPI, 0}, {JUMPDEST, 0}}, 0, 0, STOPPED, false, false, nil},
+	{"JUMPDEST OK", []Instruction{{JUMPDEST, 0}}, 0, 0, STOPPED, false, false, nil},
+	{"RETURN OK", []Instruction{{RETURN, 0}}, 20, 0, RETURNED, false, false, nil},
+	{"REVERT OK", []Instruction{{REVERT, 0}}, 20, 0, REVERTED, false, false, nil},
+	{"PC OK", []Instruction{{PC, 0}}, 0, 0, STOPPED, false, false, nil},
+	{"STOP OK", []Instruction{{STOP, 0}}, 0, 0, STOPPED, false, false, nil},
+	{"SLOAD OK", []Instruction{{PUSH1, 0}, {SLOAD, 0}}, 0, 0, STOPPED, false, false,
+		func(mockStateDB *vm.MockStateDB) {
+			mockStateDB.EXPECT().GetState(common.Address{0}, common.Hash{0}).Return(common.Hash{0}).Times(1)
+		}},
+	{"SLOAD Berlin OK", []Instruction{{PUSH1, 0}, {SLOAD, 0}}, 0, 0, STOPPED, true, false,
+		func(mockStateDB *vm.MockStateDB) {
+			mockStateDB.EXPECT().GetState(common.Address{0}, common.Hash{0}).Return(common.Hash{0}).Times(1)
+			mockStateDB.EXPECT().SlotInAccessList(common.Address{0}, common.Hash{0}).Return(true, true).Times(1)
+		}},
+	{"SLOAD Berlin no slot OK", []Instruction{{PUSH1, 0}, {SLOAD, 0}}, 0, 0, STOPPED, true, false,
+		func(mockStateDB *vm.MockStateDB) {
+			mockStateDB.EXPECT().GetState(common.Address{0}, common.Hash{0}).Return(common.Hash{0}).Times(1)
+			mockStateDB.EXPECT().SlotInAccessList(common.Address{0}, common.Hash{0}).Return(false, false).Times(1)
+			mockStateDB.EXPECT().AddSlotToAccessList(common.Address{0}, common.Hash{0}).Times(1)
+		}},
 }
 
 func addOKOpCodes(tests []OpcodeTest) []OpcodeTest {
@@ -193,46 +210,45 @@ func addOKOpCodes(tests []OpcodeTest) []OpcodeTest {
 		for j := 0; j < dataNum; j++ {
 			code = append(code, Instruction{DATA, 1})
 		}
-		addedTests = append(addedTests, OpcodeTest{i.String() + " execution", code, false, 0, STOPPED, false, false})
+		addedTests = append(addedTests, OpcodeTest{i.String() + " execution", code, 20, 0, STOPPED, false, false, nil})
 	}
 	var opCodes []OpCode
 	opCodes = append(opCodes, getInstructions(DUP1, SWAP16)...)
+	opCodes = append(opCodes, getInstructions(ADD, SAR)...)
+	opCodes = append(opCodes, getInstructions(SWAP1_POP_SWAP2_SWAP1, PUSH1_DUP1)...)
+	opCodes = append(opCodes, getInstructions(PUSH1_PUSH1, SWAP1_POP)...)
+	opCodes = append(opCodes, getInstructions(SWAP2_SWAP1, DUP2_LT)...)
 	for _, opCode := range opCodes {
 		code := []Instruction{{opCode, 1}}
-		addedTests = append(addedTests, OpcodeTest{opCode.String() + " execution", code, false, 0, STOPPED, false, false})
+		addedTests = append(addedTests, OpcodeTest{opCode.String() + " execution", code, 20, 0, STOPPED, false, false, nil})
 	}
 	return addedTests
 }
 
 func TestOK(t *testing.T) {
 
-	// Create a dummy contract
-	addr := vm.AccountRef{}
-	contract := vm.NewContract(addr, addr, big.NewInt(0), 1<<63)
+	var mockCtrl *gomock.Controller
+	var mockStateDB *vm.MockStateDB
 
 	tests := addOKOpCodes(opcodeTests)
 
 	for _, test := range tests {
 
-		// Create execution context.
-		ctxt := context{
-			code:     test.code,
-			data:     nil,
-			callsize: *uint256.NewInt(uint64(0)),
-			stack:    NewStack(),
-			memory:   NewMemory(),
-			readOnly: true,
-			contract: contract,
+		if test.mockCalls != nil {
+			mockCtrl = gomock.NewController(t)
+			mockStateDB = vm.NewMockStateDB(mockCtrl)
+			test.mockCalls(mockStateDB)
+		} else {
+			mockStateDB = nil
 		}
-
-		// Reset the context.
-		ctxt.pc = 0
-		ctxt.status = RUNNING
-		ctxt.contract.Gas = 1 << 31
-		ctxt.stack.stack_ptr = 20
+		ctxt := getContext(test.code, make([]byte, 0), test.stackPtrPos, mockStateDB, test.isBerlin, test.isLondon)
 
 		// Run testing code
 		run(&ctxt)
+
+		if test.mockCalls != nil {
+			mockCtrl.Finish()
+		}
 
 		// Check the result.
 		if ctxt.status != test.endStatus {
