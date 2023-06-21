9c5132fd78c1e692ed2dfba586ded465f712980a
Andrei Ivasko <andrei@fantom.foundation>
2023-06-21 12:31:23 +0300

    Add geth prefix to crypto/secp256k1 library  (#37)
    * fix secp156k1 dup issue
    
    * rename secp256k1 prefix to geth
    
    * replace geth_geth with geth prefix

--- crypto/secp256k1/secp256.go
+++ crypto/secp256k1/secp256.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-// +build !gofuzz
-// +build cgo
+//go:build !gofuzz && cgo
+// +build !gofuzz,cgo
 
 // Package secp256k1 wraps the bitcoin secp256k1 C library.
 package secp256k1
@@ -31,8 +31,8 @@ package secp256k1
 #include "ext.h"
 
 typedef void (*callbackFunc) (const char* msg, void* data);
-extern void secp256k1GoPanicIllegal(const char* msg, void* data);
-extern void secp256k1GoPanicError(const char* msg, void* data);
+extern void geth_secp256k1GoPanicIllegal(const char* msg, void* data);
+extern void geth_secp256k1GoPanicError(const char* msg, void* data);
 */
 import "C"
 
@@ -46,9 +46,9 @@ var context *C.secp256k1_context
 
 func init() {
 	// around 20 ms on a modern CPU.
-	context = C.secp256k1_context_create_sign_verify()
-	C.secp256k1_context_set_illegal_callback(context, C.callbackFunc(C.secp256k1GoPanicIllegal), nil)
-	C.secp256k1_context_set_error_callback(context, C.callbackFunc(C.secp256k1GoPanicError), nil)
+	context = C.geth_secp256k1_context_create_sign_verify()
+	C.geth_secp256k1_context_set_illegal_callback(context, C.callbackFunc(C.geth_secp256k1GoPanicIllegal), nil)
+	C.geth_secp256k1_context_set_error_callback(context, C.callbackFunc(C.geth_secp256k1GoPanicError), nil)
 }
 
 var (
@@ -75,16 +75,16 @@ func Sign(msg []byte, seckey []byte) ([]byte, error) {
 		return nil, ErrInvalidKey
 	}
 	seckeydata := (*C.uchar)(unsafe.Pointer(&seckey[0]))
-	if C.secp256k1_ec_seckey_verify(context, seckeydata) != 1 {
+	if C.geth_secp256k1_ec_seckey_verify(context, seckeydata) != 1 {
 		return nil, ErrInvalidKey
 	}
 
 	var (
 		msgdata   = (*C.uchar)(unsafe.Pointer(&msg[0]))
-		noncefunc = C.secp256k1_nonce_function_rfc6979
-		sigstruct C.secp256k1_ecdsa_recoverable_signature
+		noncefunc = C.geth_secp256k1_nonce_function_rfc6979
+		sigstruct C.geth_secp256k1_ecdsa_recoverable_signature
 	)
-	if C.secp256k1_ecdsa_sign_recoverable(context, &sigstruct, msgdata, seckeydata, noncefunc, nil) == 0 {
+	if C.geth_secp256k1_ecdsa_sign_recoverable(context, &sigstruct, msgdata, seckeydata, noncefunc, nil) == 0 {
 		return nil, ErrSignFailed
 	}
 
@@ -93,7 +93,7 @@ func Sign(msg []byte, seckey []byte) ([]byte, error) {
 		sigdata = (*C.uchar)(unsafe.Pointer(&sig[0]))
 		recid   C.int
 	)
-	C.secp256k1_ecdsa_recoverable_signature_serialize_compact(context, sigdata, &recid, &sigstruct)
+	C.geth_secp256k1_ecdsa_recoverable_signature_serialize_compact(context, sigdata, &recid, &sigstruct)
 	sig[64] = byte(recid) // add back recid to get 65 bytes sig
 	return sig, nil
 }
